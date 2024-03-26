7d2797876114ed858cd5e42b26e2804e7e1a4f64
AviGondker <137519603+AviGondker@users.noreply.github.com>
2024-03-26 20:14:47 +0000

    Add files via upload
    updates files

--- Offboarding/dev/GUI_test.ps1
+++ Offboarding/dev/GUI_test.ps1
@@ -1,11 +1,9 @@
 Import-Module ActiveDirectory
 
 # Initialise Variables
-#$logpath = "C:\scripts\Offboarding\Logs\"
-#$date = [datetime]::Today.ToString('dd-MM-yyyy')
-#$ActionLog = @()
-#$VerbosePreference = "Continue"
-#$user = 
+$LogFolder = "C:\temp\Logs\"
+$date = [datetime]::Today.ToString('dd-MM-yyyy')
+$VerbosePreference = "Continue"
 
 #Creates the interface
 Add-Type -AssemblyName System.Windows.Forms
@@ -17,7 +15,7 @@ $main_form.Width = 425
 $main_form.Height = 400
 $main_form.AutoSize = $false
 $main_form.StartPosition = 'CenterScreen'
-#$main_form.Topmost = $true
+$main_form.Topmost = $true
 
 #Add the Ticket Reference label text
 $TicketText = New-Object System.Windows.Forms.Label
@@ -68,13 +66,6 @@ $processButton.Size = New-Object System.Drawing.Size(380,40)
 $processButton.Text = "Process Account"
 $main_form.Controls.Add($processButton)
 
-#TESTING AREA Account detection
-$Account_testLabel = New-Object System.Windows.Forms.Label
-$Account_testLabel.Text = "Account you have selected : "
-$Account_testLabel.Location  = New-Object System.Drawing.Point(10,300)
-$Account_testLabel.AutoSize = $true
-$main_form.Controls.Add($Account_testLabel)
-
 ## Add the View Logs Button
 $viewLogsButton = New-Object System.Windows.Forms.Button
 $viewLogsButton.Location = New-Object System.Drawing.Point(10,240)
@@ -101,16 +92,74 @@ $main_form.Controls.Add($CopyrightLabel)
 # What happens when you click the Find User button
 $finduserButton.Add_Click(
 {
-$user = $UsernameField.Text
-$Account_confirmLabel.Text = "Account you have selected : $user"
+$sam = $UsernameField.Text
+$user = $(try {Get-ADUser $sam -properties distinguishedName, displayName -server dc01.homenet.local} catch {$null})
+$dn = $user.distinguishedName
+$din = $user.displayName
+
+If ($user -ne $Null) {
+
+    $Account_confirmLabel.Text = "Found account for $din"
+} Else {
+    $Account_confirmLabel.Text =  "WARNING : Could not find account for $sam"
+}
 }
 )
 
-# TESTING What happens when you click the Process Account button
+###TESTING AREA###
+## 
+#Account detection
+$Account_testLabel = New-Object System.Windows.Forms.Label
+$Account_testLabel.Text = "Account you have selected : "
+$Account_testLabel.Location  = New-Object System.Drawing.Point(10,300)
+$Account_testLabel.AutoSize = $true
+$main_form.Controls.Add($Account_testLabel)
+
+#What happens when you click the Process Account button
 $processButton.Add_Click(
 {
-$user = $UsernameField.Text
-$Account_testLabel.Text = "Account you have selected : $user"
+$sam = $UsernameField.Text
+$user = $(try {Get-ADUser $sam -properties distinguishedName, displayName -server dc01.homenet.local} catch {$null})
+$dn = $user.distinguishedName
+$din = $user.displayName
+$ticketRef = $TicketField.Text
+#Starts Session logging
+Start-Transcript -Path "$LogFolder\session-$date.log"
+
+# Disable the account
+Disable-ADAccount $dn
+Write-Verbose ($din + "'s Active Directory account is disabled.")
+#$ActionLog += $ad_user.username + " Account Disabled"
+
+# Add the relevant info to the leavers description on the account's properties page, clean out manager etc
+Set-ADUser $dn -Description ("Leaver : $ticketRef - $date")
+Set-ADUser -Identity $dn -Clear Manager
+Write-Verbose  ("* " + $din + "'s Active Directory Description updated.")
+#$ActionLog += $ad_user.username + " Attributes Updated - Description"
+
+# Remove the LoginScript attributes
+Set-ADUser -Identity $dn -Clear ScriptPath
+Write-Verbose  ("* " + $din + "'s Active Directory Loginscript removed.")
+#$ActionLog += $ad_user.username + " Attributes Updated - Loginscript"
+
+# Strip the permissions from the account
+Get-ADUser $dn -Properties MemberOf | Select-Object -Expand MemberOf | ForEach-Object {Remove-ADGroupMember $_ -member $dn -Confirm:$false} 
+Write-Verbose  ("* " + $din + "'s Active Directory group memberships (permissions) stripped from account")
+#$ActionLog += $user.username + " Active Directory group memberships (permissions) stripped from account"
+
+# Set Account Expiry Date
+Set-ADAccountExpiration -Identity $dn -DateTime $date
+Write-Verbose  ("* " + $din + "'s Active Directory Account set to expire")
+#$ActionLog += $user.username + " Active Directory accout set to expire"
+
+# Move the account to the Disabled Users OU
+Move-ADObject -Identity $dn -TargetPath "OU=Leavers, OU=Disabled Accounts, OU=Decommissioned Computers, DC=homenet, DC=local"
+Write-Verbose  ("* " + $din + "'s Active Directory account moved to 'Leavers' OU")
+#$SuccessLog += $user.username + "Active Directory account moved to 'Leavers' OU"
+
+$ActionLog | out-file -FilePath  $LogFolder\DisableAaccount-$date.log -Force
+Stop-Transcript
+[System.Windows.Forms.MessageBox]::Show("Account for $din has been processed. Please review Logs if errors appear")
 }
 )
 
