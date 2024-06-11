70ecf603baec149a0f34b3d3bd86a91a5618895a
AviGondker <avi.gondker@gmail.com>
2024-06-11 09:29:55 +0100

    dev folders

---
+++ Offboarding/dev/offboarding-dev.bat
@@ -0,0 +1,2 @@
+﻿@ECHO OFF
+START powershell -ExecutionPolicy Bypass -windowstyle hidden -file .\offboarding-dev.ps1
--- Offboarding/dev/offboarding-dev.ps1
+++ Offboarding/dev/offboarding-dev.ps1
@@ -18,9 +18,10 @@ Active Directory Section:
 # Implement a tick box for "Holding" - will then move to Holding and add additional/different notes etc?
 # Exports a list of the user's group memberships (permissions) to an Excel file in a specified directory?
 
-Version History
-V1 - Script Only
-V2 - 03/05/2024 - Added a GUI
+# Version History
+# V1 - Script Only
+# V2 - 03/05/2024 - Added a GUI
+# V2.5 - 11/06/2024 - Added Holding Button checkbox and associated code
 
 ############################################################################################################>
 
@@ -30,6 +31,7 @@ Import-Module ActiveDirectory
 $LogFolder = "C:\offboarding\logs"
 $VerbosePreference = "Continue"
 $ActionLog = @()
+$date = [datetime]::Today.ToString('dd-MM-yyyy')
 
 #Creates the interface
 Add-Type -AssemblyName System.Windows.Forms
@@ -45,7 +47,7 @@ $main_form.Topmost = $true
 
 #Add the Ticket Reference label text
 $TicketText = New-Object System.Windows.Forms.Label
-$TicketText.Text = "Please enter the ServiceDesk Ticket Ref below"
+$TicketText.Text = "1. Please enter the full ticket ref eg ITR-12345"
 $TicketText.Location  = New-Object System.Drawing.Point(10,30)
 $TicketText.AutoSize = $true
 $main_form.Controls.Add($TicketText)
@@ -59,14 +61,14 @@ $main_form.Controls.Add($TicketField)
 
 # Add the FindUser Button
 $finduserButton = New-Object System.Windows.Forms.Button
-$finduserButton.Location = New-Object System.Drawing.Point(250, 80)
-$finduserButton.Size = New-Object System.Drawing.Size(100,50)
+$finduserButton.Location = New-Object System.Drawing.Point(200, 108)
+$finduserButton.Size = New-Object System.Drawing.Size(80,25)
 $finduserButton.Text = 'FindUser'
 $main_form.Controls.Add($finduserButton)
 
 #Add the Username label text
 $UsernameText = New-Object System.Windows.Forms.Label 
-$UsernameText.Text = "Please enter the Username below, eg ' auser '"
+$UsernameText.Text = "2. Please enter the Username, eg ' auser '"
 $UsernameText.Location  = New-Object System.Drawing.Point(10,90)
 $UsernameText.AutoSize = $true
 $main_form.Controls.Add($UsernameText)
@@ -80,21 +82,29 @@ $main_form.Controls.Add($UsernameField)
 
 #Add the Account Confirmation
 $Account_confirmLabel = New-Object System.Windows.Forms.Label
-$Account_confirmLabel.Text = "Account you have selected : "
+$Account_confirmLabel.Text = "Confirmation of Account to be processed : "
 $Account_confirmLabel.Location  = New-Object System.Drawing.Point(10,150)
 $Account_confirmLabel.AutoSize = $true
 $main_form.Controls.Add($Account_confirmLabel)
 
+## Add the Holding Account Checkbox
+$holdingcheckbox = new-object System.Windows.Forms.CheckBox
+$holdingcheckbox.Location = new-object System.Drawing.Size(10,175)
+$holdingcheckbox.Size = new-object System.Drawing.Size(350,20)
+$holdingcheckbox.Text = "Select here if this account needs to be put in holding OU"
+$holdingcheckbox.Checked = $false
+$main_form.Controls.Add($holdingcheckbox) 
+
 # Process the Account Button
 $processButton = New-Object System.Windows.Forms.Button
-$processButton.Location = New-Object System.Drawing.Point(10,180)
-$processButton.Size = New-Object System.Drawing.Size(380,40)
+$processButton.Location = New-Object System.Drawing.Point(10,210)
+$processButton.Size = New-Object System.Drawing.Size(390,40)
 $processButton.Text = "Process Account"
 $main_form.Controls.Add($processButton)
 
 # Add the Exit Button
 $exitButton = New-Object System.Windows.Forms.Button
-$exitButton.Location = New-Object System.Drawing.Point(140,240)
+$exitButton.Location = New-Object System.Drawing.Point(140,260)
 $exitButton.Size = New-Object System.Drawing.Size(150,50)
 $exitButton.Text = 'EXIT'
 $exitButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
@@ -117,9 +127,9 @@ $din = $user.displayName
 
 If ($user -ne $Null) {
 
-    $Account_confirmLabel.Text = "Found account for $din, $sam"
+    $Account_confirmLabel.Text = "Found account for -  $sam, $din"
 } Else {
-    $Account_confirmLabel.Text =  "WARNING : Could not find account for $din, $sam"
+    $Account_confirmLabel.Text =  "WARNING : Could not find account for - $sam"
 }
 }
 )
@@ -135,10 +145,21 @@ $processButton.Add_Click(
 $sam = $UsernameField.Text
 $user = $(try {Get-ADUser $sam -properties distinguishedName, displayName} catch {$null})
 $din = $user.displayName
-$dn = $user.distinguishedName
+$dn= $user.distinguishedName
 $ticketRef = $TicketField.Text
-$date = [datetime]::Today.ToString('dd-MM-yyyy')
 
+if ($holdingcheckbox.Checked = $true)
+{
+# Add the relevant info to the leavers description on the account's properties page
+Set-ADUser $dn -Description ("HOLDING - $ticketRef - $date")
+
+# Move the account to the Holding OU
+Move-ADObject -Identity $dn -TargetPath "OU=Holding,OU=Disabled Accounts,OU=Decommissioned Computers,DC=sbs,DC=ox,DC=ac,DC=uk"
+Write-Verbose  ("* " + $din + "'s Active Directory account moved to 'Holding' OU")
+$global:ActionLog += $din + " Active Directory account moved to 'Holding' OU"
+
+}
+else{
 # Set Account Expiry Date
 Set-ADAccountExpiration -Identity $dn -DateTime $date
 Write-Verbose  ("* " + $din + "'s Active Directory Account set to expire")
@@ -168,6 +189,7 @@ Move-ADObject -Identity $dn -TargetPath "OU=Leavers,OU=Disabled Accounts,OU=Deco
 Write-Verbose  ("* " + $din + "'s Active Directory account moved to 'Leavers' OU")
 $global:ActionLog += $din + " Active Directory account moved to 'Leavers' OU"
 
+}
 $global:ActionLog | out-file -FilePath  $LogFolder\DisableAccount-$date.log -Force
 
 #Uncomment if you have re-enabled session logging for debugging
